630cf7c657e6a102cd5c4f624bf842e0a46dd1ca
cgyireh <c.gyireh@alustudent.com>
2023-07-09 09:34:31 -0700

    edits

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,16 +3,3 @@
 
 import requests
 from sys import argv
-
-def display():
-    """Fetching API data"""
-    users = requests.get("http://jsonplaceholder.typicode.com/users")
-"""Iterating over the JSON response to find user with the specified id"""
-for u in users.json():
-        if u.get('id') == int(argv[1]):
-            EMPLOYEE_NAME = (u.get('name'))
-            break
-TOTAL_NUM_OF_TASKS = 0
-NUMBER_OF_DONE_TASKS = 0
-TASK_TITLE = []
-
