e0d28dcee8fb57efc8a0d7edaa29c25c17b5a074
cgyireh <c.gyireh@alustudent.com>
2023-07-09 09:32:53 -0700

    edits

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -15,19 +15,4 @@ for u in users.json():
 TOTAL_NUM_OF_TASKS = 0
 NUMBER_OF_DONE_TASKS = 0
 TASK_TITLE = []
-todos = requests.get("http://jsonplaceholder.typicode.com/todos")
-for t in todos.json():
-        if t.get('userId') == int(argv[1]):
-            TOTAL_NUM_OF_TASKS += 1
-            if t.get('completed') is True:
-                    NUMBER_OF_DONE_TASKS += 1
-                    TASK_TITLE.append(t.get('title'))
-print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
-                                                          NUMBER_OF_DONE_TASKS,
-                                                          TOTAL_NUM_OF_TASKS))
-for task in TASK_TITLE:
-        print("\t {}".format(task))
 
-
-if __name__ == "__main__":
-    display()
