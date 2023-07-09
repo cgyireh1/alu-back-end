3056e155421d4fe8883cf0acf45a3c81a0c1222f
cgyireh <c.gyireh@alustudent.com>
2023-07-09 08:43:51 -0700

    edits

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ from sys import argv
 def display():
     """Fetching API data"""
     users = requests.get("http://jsonplaceholder.typicode.com/users")
-"""Iterating over the JSON response to fin user with the specified id"""
+"""Iterating over the JSON response to find user with the specified id"""
 for u in users.json():
         if u.get('id') == int(argv[1]):
             EMPLOYEE_NAME = (u.get('name'))
